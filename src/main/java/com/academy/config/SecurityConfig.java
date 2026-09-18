bb4a027e761a87b46448b5a9f84f96d046d4f536
Kaushal Raj Singh <rajkaushal02@gmail.com>
2026-09-17 23:50:01 -0700

    update done on cors config

--- src/main/java/com/academy/config/SecurityConfig.java
+++ src/main/java/com/academy/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
 
 		CorsConfiguration configuration = new CorsConfiguration();
 
-		configuration.setAllowedOrigins(List.of("http://localhost:3000","https://maa-sharda-nu.vercel.app"));
+		configuration.setAllowedOrigins(List.of("http://localhost:3000","https://maa-sharda-nu.vercel.app","https://www.maashardaacademy.org","https://maashardaacademy.org"));
 
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 
